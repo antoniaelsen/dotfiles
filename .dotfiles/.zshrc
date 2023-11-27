07f9a7f4f2236f649cbdc0736f971a73abb9074c
Antonia Elsen <antoniae@Antonias-MacBook-Pro.local>
2023-11-26 22:35:05 -0800

    fix: fix brew, aliases

--- .dotfiles/.zshrc
+++ .dotfiles/.zshrc
@@ -16,6 +16,7 @@ plugins=(
 )
 
 source $ZSH/oh-my-zsh.sh
+source ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
 
 
 ##############################################################################
--- brew.sh
+++ brew.sh
@@ -23,17 +23,7 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi  # TODO(antoniae)
 brew install grep
 brew install gpg
 brew install openssh
